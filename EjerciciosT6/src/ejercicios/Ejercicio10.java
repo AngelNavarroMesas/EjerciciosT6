c9dd107b24deab248711a1895439abc8d941d8df
anavarro <anavarro@106-30.info2>
2023-02-23 14:37:19 +0100

    commit ejercicio10

--- EjerciciosT6/src/ejercicios/Ejercicio10.java
+++ EjerciciosT6/src/ejercicios/Ejercicio10.java
@@ -1,11 +1,44 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char[] conju1 = {'e', 'i','k','m','p','q','r','s','t','u','v'};
+		char[] conju2 = {'p', 'v','i','u','m','t','e','r','k','q','s'};
+		char[] c;
+		String palabra;
 		
+		System.out.println("Introduzca una palabra para descodificarla");
+		palabra = sc.nextLine();
+		c = palabra.toCharArray();
 		
+		for(int i=0;i<c.length;i++) {
+			c[i]=descodificado(conju1, conju2,c[i]);
+		}
 		
+		System.out.println(palabra = String.valueOf(c));
+		
+	}
+	
+	public static char descodificado(char[] conju1, char[] conju2, char c) {
+		int x=0;
+		
+		do {
+			if(conju2[x]==c){
+				c =conju1[x];
+				break;
+			}else if(x==conju1.length-1){
+				break;
+			}else {
+				x++;
+			}
+			
+		}while(true);
+		
+		return c;
 	}
 
 }
